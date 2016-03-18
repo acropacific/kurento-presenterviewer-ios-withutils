b853e9308632633909d827a49b7cb454dd4fe75e
Mark Veenstra <mark@mobilea.nl>
2016-03-18 11:41:35 +0100

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+# Kurento One2Many iOS Demo
+
+Adapted from [Kurento's One2Many Demo](https://github.com/Kurento/kurento-tutorial-node/tree/master/kurento-one2many-call)
+And adapted from [m59peacemaker](https://github.com/m59peacemaker/kurento-one2many-iOS)
+
+Start [Kurento Media Server](https://www.kurento.org/docs/current/installation_guide.html) on port 8888 (default).
+
+Enter ./server and run `npm install` then start the server with `node server`. If necessary change the WS URI to Kurento installation in `server.js`.
+
+Open ./www/index.js and set line 1 `var host = ` to the ip/domain where kms and the node server are running.
+
+### Test in browser
+
+Serve ./www/index.html
+
+```sh
+cd www
+live-server --port=8081
+```
+
+Visit localhost:8081 in a browser.
+
+### Test on iOS device
+
+Connect your iOS device to the Apple computer.
+
+```sh
+npm install xcode -g (globally or locally)
+cordova platform add ios
+cordova run ios --device
+```
+
+[This is the Dockerfile I use](http://pastebin.com/3ih5cqA2) for Kurento Media Server. I run it with --net=host to make things easier.
+
+### Problem to tackle
+
+If you have everything up and running. You'll notice that:
+
+* Being a presenter on iOS and a viewer in Firefox does work
+* Being a presenter on Firefox and a viewer in iOS does work
+* Being a presenter on Firefox and a viewer in Firefox does work
+* Being a presenter on iOS and a viewer in iOS does NOT work
+
+Hopefully someone can tackle this issue.
---
+++ www/js/index.js
@@ -0,0 +1,187 @@
+var host = 'localhost'; // the domain or IP where the node server and kurento media server are running
+
+function deviceReady() {
+  return new Promise(function(resolve, reject) {
+    if (window.cordova) {
+      document.addEventListener("deviceready", function() {
+        resolve();
+      });
+    } else {
+      resolve();
+    }
+  });
+}
+
+function isDevice(device) {
+  return window.device && window.device.platform === device;
+}
+
+
+deviceReady().then(function() {
+  if (isDevice('iOS')) {
+    cordova.plugins.iosrtc.registerGlobals();
+    window.getUserMedia = navigator.getUserMedia.bind(navigator);
+  }
+  stuff();
+});
+
+function stuff() {
+
+  var ws = new WebSocket('ws://'+host+':8080/one2many');
+  var video;
+  var webRtcPeerViewer, webRtcPeerPresenter;
+
+  videoPresenter = document.getElementById('videoPresenter');
+  videoViewer = document.getElementById('videoViewer');
+
+  document.getElementById('call').addEventListener('click', function() { presenter(); } );
+  document.getElementById('viewer').addEventListener('click', function() { viewer(); } );
+  document.getElementById('terminate').addEventListener('click', function() { stop(); } );
+
+  window.onbeforeunload = function() {
+    ws.close();
+  }
+
+  ws.onmessage = function(message) {
+    var parsedMessage = JSON.parse(message.data);
+
+    switch (parsedMessage.id) {
+    case 'presenterResponse':
+      presenterResponse(parsedMessage);
+      break;
+    case 'viewerResponse':
+      viewerResponse(parsedMessage);
+      break;
+    case 'stopCommunication':
+      dispose();
+      break;
+    case 'iceCandidatePresenter':
+      webRtcPeerPresenter.addIceCandidate(parsedMessage.candidate);
+      break;
+    case 'iceCandidateViewer':
+      webRtcPeerViewer.addIceCandidate(parsedMessage.candidate);
+      break;
+    default:
+
+    }
+  }
+
+  function presenterResponse(message) {
+    if (message.response != 'accepted') {
+      var errorMsg = message.message ? message.message : 'Unknow error';
+      dispose();
+    } else {
+      webRtcPeerPresenter.processAnswer(message.sdpAnswer);
+    }
+  }
+
+  function viewerResponse(message) {
+    if (message.response != 'accepted') {
+      var errorMsg = message.message ? message.message : 'Unknow error';
+      dispose();
+    } else {
+      webRtcPeerViewer.processAnswer(message.sdpAnswer);
+    }
+  }
+
+  function presenter() {
+    if (!webRtcPeerPresenter) {
+
+      var options = {
+        localVideo: videoPresenter,
+        onicecandidate : onIceCandidatePresenter
+      };
+
+      webRtcPeerPresenter = kurentoUtils.WebRtcPeer.WebRtcPeerSendonly(options, function(error) {
+        if(error) return onError(error);
+
+        this.generateOffer(onOfferPresenter);
+      });
+    }
+  }
+
+  function onOfferPresenter(error, offerSdp) {
+    if (error) return onError(error);
+
+    var message = {
+      id : 'presenter',
+      sdpOffer : offerSdp
+    };
+    sendMessage(message);
+  }
+
+  function viewer() {
+    if (webRtcPeerViewer) { return; }
+
+    var options = {
+      remoteVideo: videoViewer,
+      onicecandidate : onIceCandidateViewer
+    };
+
+    deviceReady().then(function() {
+      if (isDevice('iOS')) {
+        options.connectionConstraints = {
+          offerToReceiveAudio: true,
+          offerToReceiveVideo: true
+        };
+      }
+
+      webRtcPeerViewer = kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options, function(error) {
+        if(error) return onError(error);
+        this.generateOffer(onOfferViewer);
+      });
+    });
+  }
+
+  function onOfferViewer(error, offerSdp) {
+    if (error) return onError(error)
+
+    var message = {
+      id : 'viewer',
+      sdpOffer : offerSdp
+    };
+    sendMessage(message);
+  }
+
+  function onIceCandidatePresenter(candidate) {
+       var message = {
+          id : 'onIceCandidatePresenter',
+          candidate : candidate
+       }
+       sendMessage(message);
+  }
+
+  function onIceCandidateViewer(candidate) {
+    var message = {
+      id : 'onIceCandidateViewer',
+      candidate : candidate
+    }
+    sendMessage(message);
+  }
+
+  function stop() {
+    if (webRtcPeerViewer || webRtcPeerPresenter) {
+      var message = {
+          id : 'stop'
+      }
+      sendMessage(message);
+      dispose();
+    }
+  }
+
+  function dispose() {
+    if (webRtcPeerViewer) {
+      webRtcPeerViewer.dispose();
+      webRtcPeerViewer = null;
+    }
+    if (webRtcPeerPresenter) {
+      webRtcPeerPresenter.dispose();
+      webRtcPeerPresenter = null;
+    }
+  }
+
+  function sendMessage(message) {
+    var jsonMessage = JSON.stringify(message);
+    ws.send(jsonMessage);
+  }
+}
